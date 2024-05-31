d09382cbe81a93b33714bfd2ab870a479e239046
Gökhan Agkurt <gokhan@Air-von-Gokhan.fritz.box>
2024-05-31 21:52:17 +0200

    add groupList

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,13 @@ import GlobalStyle from "../styles";
 
 
 import useLocalStorageState from "use-local-storage-state";
-import { initialStudents } from "@/lib/data";
+import { initialStudents, initialGroups } from "@/lib/data";
 
 export default function App({ Component, pageProps }) {
 
   const [students, setStudents] = useLocalStorageState("students", { defaultValue: initialStudents });
+  const [groups, setGroups] = useLocalStorageState("groups", { defaultValue: initialGroups });
+
 
   function handleAddStudent(newStudent) {
     setStudents([...students, newStudent])
@@ -26,7 +28,14 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} students={students} onAddStudent={handleAddStudent} onDeleteStudent={handleDeleteStudent} onEditStudent={handleEditStudent} />
+      <Component {...pageProps}
+        students={students}
+        onAddStudent={handleAddStudent}
+        onDeleteStudent={handleDeleteStudent}
+        onEditStudent={handleEditStudent}
+
+        groups={groups}
+      />
     </>
   );
 }
